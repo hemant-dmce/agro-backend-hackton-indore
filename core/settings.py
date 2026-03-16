28b9f2f0d600d9439abf947d898a99bc7936bf57
GitHemant-coder <patilhemant1103@gmail.com>
2026-03-16 18:41:27 +0530

    fix the bug

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-fbw!-dvfdhoteqv=-^04588^rn$&b*y%bn9oiy5wj1_6*+x5*8'
 
@@ -98,11 +98,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
 USE_TZ = True
 
 # Static files (important for deployment)
@@ -158,12 +156,3 @@ EMAIL_USE_TLS = False
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'AgroCast <noreply@agrocast.com>'
-
-# Gmail SMTP (optional for production)
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'your-email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'your-app-password'
